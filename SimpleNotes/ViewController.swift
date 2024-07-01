09f533393d0c3affd4c06d73d9a9ece3bc97a74a
Kamil <biktineyev@hotmail.com>
2024-07-01 17:29:33 +0300

    Initial Commit

---
+++ SimpleNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleNotes
+//
+//  Created by Kamil Biktineyev on 01.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
